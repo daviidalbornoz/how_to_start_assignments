a38a870a24cda9454dbac5e0c41f310d7be59827
David Albornoz <albornozd@usf.edu>
2026-02-01 14:29:03 -0500

    Updated README.md, index.html with header,  js file with new code.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # coding challenge #xxxx
 answers for cd=oding challenge #xxxx
+
+Here is another example in the README.md file. 
--- assignment_1.js
+++ assignment_1.js
@@ -2,3 +2,6 @@
 
 const f_name = "David"
  console.log(`Hello ${f_name}.`)
+
+ let number = 42
+ console.log(`Your number is: ${number}.`)
